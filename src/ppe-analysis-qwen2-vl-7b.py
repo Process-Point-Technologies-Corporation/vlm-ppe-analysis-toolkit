ab25c1ce50c891daff7d30c2e29ab28d9c4fdf6e
shreyanssj.pptc <shreyans.jain@pptcorporation.com>
2024-11-12 22:27:27 +0530

    Updating Qwen2 code

--- src/ppe-analysis-qwen2-vl-7b.py
+++ src/ppe-analysis-qwen2-vl-7b.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from PIL import Image
 from IPython.display import HTML, display
 from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
+from visualize_result import generate_html, save_html
+from qwen_vl_utils import process_vision_info
 import torch
 import time
 
@@ -10,15 +12,21 @@ import time
 def load_model(model_name): #, model_cache_dir=r"C:\Users\shrey\.cache\huggingface\hub"):
     """Loads the model and tokenizer from cache or downloads them"""
     # device = "cuda" if torch.cuda.is_available() else "cpu"
-    processor = AutoProcessor.from_pretrained(
-        model_name, 
-        # cache_dir=model_cache_dir
-    )
     model = Qwen2VLForConditionalGeneration.from_pretrained(
         model_name, 
+        torch_dtype="auto",
         device_map="auto", 
         # cache_dir=model_cache_dir
     )
+    min_pixels = 256 * 28 * 28
+    max_pixels = 1280 * 28 * 28
+    processor = AutoProcessor.from_pretrained(
+        model_name, 
+        min_pixels=min_pixels, 
+        max_pixels=max_pixels
+        # cache_dir=model_cache_dir
+    )
+    
     # model.to(device)
     return model, processor
 
@@ -27,14 +35,14 @@ def analyze_image(image_path, model, processor):
     """Analyze single image with Given Model"""
     try:
         # Encode image
-        image_name = Image.open(image_path)
+        image_name = Image.open(image_path).convert('RGB')
         
         # Create messages for the model consumption
         messages = [
             {
                 "role": "user", 
                 "content": [
-                    {"type": "image"}, 
+                    {"type": "image", "image": image_name}, 
                     {
                         "type": "text", 
                         "text": "Please describe in particular, the protective equipment being worn, if present."
@@ -45,11 +53,22 @@ def analyze_image(image_path, model, processor):
         
         # Prepare inputs for the model
         text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        model_inputs = processor(text=[text],images=[image_name], padding=True, return_tensors="pt").to("cuda" if torch.cuda.is_available() else "cpu")
+        image_inputs, _ = process_vision_info(messages)
+        model_inputs = processor(
+            text=[text],
+            images=image_inputs,
+            padding=True, 
+            return_tensors="pt"
+        ).to("cuda" if torch.cuda.is_available() else "cpu")
         
         # Generate response
-        generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=512)
-        response_text = processor.decode(generated_ids[0], skip_special_tokens=True)
+        generated_ids = model.generate(**model_inputs, max_new_tokens=512)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :] for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+        response_text = processor.batch_decode(
+            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_space=False
+        )
         
         return response_text
         
@@ -98,18 +117,21 @@ if __name__ == "__main__":
 
     # Model Save Path
     # model_cache_dir = r"C:\Users\shrey\OneDrive - Process Point Technologies\vlm-ppe-analysis-toolkit\models"
-    model_name = "Qwen/Qwen2-VL-7B-Instruct"
+    model_name = "Qwen/Qwen2-VL-7B-Instruct-GPTQ-Int4"
 
     # Load Model & Processor
     model, processor = load_model(model_name)  #, model_cache_dir)
 
+    start_time = time.time()
     # Process images
     results = process_folder(image_folder, model, processor)  # Process all images
-
+    end_time = time.time()
+    print(f'Processing {len(results)} images took {round((end_time - start_time), 3)} seconds.')
+    
     # Generate and display HTML in notebook
     html_content = generate_html(results)
     display(HTML(html_content))
 
     # Save HTML file
-    output_path = r"C:\Users\shrey\OneDrive - Process Point Technologies\vlm-ppe-analysis-toolkit\results\qwen2_vl_analysis_results.html"
-    save_html(html_content, model_name, output_path)
+    output_file = r"C:\Users\shrey\OneDrive - Process Point Technologies\vlm-ppe-analysis-toolkit\results\qwen2_vl_GPTQ_Int4_analysis_results.html"
+    save_html(html_content, model_name, output_file)
